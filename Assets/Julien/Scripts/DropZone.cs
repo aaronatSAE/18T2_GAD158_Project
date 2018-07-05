14643f1b408548a9e2f8fbc422ede3b469634869
seann888 <40022279+seann888@users.noreply.github.com>
2018-07-05 21:22:52 +1000

    Added code

--- Assets/Julien/Scripts/DropZone.cs
+++ Assets/Julien/Scripts/DropZone.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour, IDropHandler ,IPointerEnterHandler, {
+public class DropZone : MonoBehaviour, IDropHandler ,IPointerEnterHandler, IPointerExitHandler{
+    private object gameobject;
+
+    public void OnPointerEnter(PointerEventData);
+
+    public void OmPointerExit(PointerEventData) {
+    }
 
 	public void OnDrop(PointerEventData eventData) {
         Debug.Log("OnDrop to " + gameobject.name);
